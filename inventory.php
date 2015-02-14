5c38be48a934d272961f4ac401efaa8a437174b0
lees9 <lees9@onid.oregonstate.edu>
2015-02-14 15:04:48 -0800

    trying to figure out how to add movies into the tables

--- inventory.php
+++ inventory.php
@@ -11,42 +11,9 @@
       echo "Connected to Video Inventory<br><br>";
     }
 
-/*    function printTable($var){
-          $res = $var->query("SELECT * FROM Inventory");
-          echo '<table border=2">';
-          echo"<tr><td><strong>ID</strong></td><td><strong>Name</strong></td><td><strong>Category</strong></td><td><strong>Length</strong></td><td><strong>Rented</strong></td></tr>";
-          while($row = mysqli_fetch_array($res)){   
-          echo "<tr><td>"; 
-          echo $row['id'];
-          echo "</td><td>";   
-          echo $row['name'];
-          echo "</td><td>";    
-          echo $row['category'];
-          echo "</td><td>";
-          echo $row['length'];
-          echo "</td><td>";
-          if (is_null($row['rented'])){
-            echo "Available";
-          }
-          else{
-            echo "Checked out";
-          }
-          echo "</td><td>";
-          echo "<input type='submit' value='remove'>";
-          echo "</td></tr>";
-        }
-        echo "</table><br><br>";
-    }
-
-    printTable($mysqli);*/
-
-
-
-
-
     //to make the dropdown select box
     $dropres = $mysqli->query("SELECT DISTINCT category FROM Inventory");
-    echo "<form action='' method='GET' name='form_filter'>";
+    echo "<form action='' method='POST' name='form_filter'>";
     echo "<select name='value'>";
     echo "<option value='all'>All Movies</option>";
     while ($row = mysqli_fetch_array($dropres)) {
@@ -60,12 +27,25 @@
     $query = $mysqli->query("SELECT * FROM Inventory");
 
     //to filter the table with the dropdown select box
-    if (isset($_GET['value'])){
-      if ($_GET['value'] != "all"){
-          $categoryName = $_GET['value'];
+    if (isset($_POST['value'])){
+      if ($_POST['value'] != "all"){
+          $categoryName = $_POST['value'];
           $query = $mysqli->query("SELECT * FROM Inventory WHERE category='$categoryName'");
       }
     }
+
+
+    //to add to the table
+    if (isset($_POST['title']) && isset($_POST['category']) && isset($_POST['length'])){
+      $title = $_POST['title'];
+      $category = $_POST['category'];
+      $length = $_POST['length'];
+      echo $title;
+      echo $category;
+      echo $length;
+      $mov_add = $mysqli->query("INSERT INTO Inventory(name, category, length) VALUES ($title, $category, $length)");
+    }
+
 
 
     function printTable($var){
@@ -89,14 +69,14 @@
         echo "Checked out";
       }
       echo "</td><td>";
-      echo "<input type='submit' value='remove'>";
+      echo "<form action='' method='GET' name='addrem'><input type='submit' value='remove'></form>";
       echo "</td></tr>";
     }
+    echo "<tr><td><form action='' method='GET' name='deleteall'><input type='submit' value='Remove all'></form></td></tr>";
     echo "</table><br><br>";
   }
 
   printTable($query);
-
 
 
 
@@ -109,11 +89,11 @@
 <body>
 
 
-    <form action="" method="POST">
+    <form action="" method="POST" name='additions'>
       Name: <input type="text" name="title"><br><br>
       Category: <input type="text" name="category"><br><br>
       Length: <input type="number" name="length"><br><br>
-      <input type="submit" name="submit" value="Add Movie">
+      <input type="submit" name="submit">
     </form><br><br>
   </body>
   </html>
